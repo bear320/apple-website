42769871538a67d428b659a86e46be5bdfdd5fd6
bear320 <aaabear320@gmail.com>
2024-11-02 12:48:35 +0800

    feat: 🎸 Process model view and create lights

--- src/components/ModelView.tsx
+++ src/components/ModelView.tsx
@@ -1,5 +1,26 @@
-const ModelView = () => {
-  return <div>ModelView</div>;
+import { PerspectiveCamera, View } from "@react-three/drei";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`h-full w-full border-2 border-red-200 ${index === 2 ? "right-[-100%]" : ""}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+    </View>
+  );
 };
 
 export default ModelView;
