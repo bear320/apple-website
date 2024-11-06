91e785068e000cb7d71c6236cfb4e500b0bbd106
bear320 <aaabear320@gmail.com>
2024-11-06 19:47:00 +0800

    feat: 🎸 Create features section

---
+++ src/components/Features.tsx
@@ -0,0 +1,15 @@
+const Features = () => {
+  return (
+    <section className="common-padding relative h-full overflow-hidden bg-zinc">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full">
+          <h1 id="features_title" className="section-heading">
+            Explore the full story.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
