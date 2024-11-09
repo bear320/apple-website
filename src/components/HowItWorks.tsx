b4405443ed7369e41f4cacac91840446bfdc3a7a
bear320 <aaabear320@gmail.com>
2024-11-09 21:09:53 +0800

    feat: 🎸 Create how-it-works section

---
+++ src/components/HowItWorks.tsx
@@ -0,0 +1,40 @@
+import { chipImg } from "../utils";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const HowItWorks = () => {
+  useGSAP(() => {
+    gsap.from("#chip", {
+      scrollTrigger: {
+        trigger: "#chip",
+        start: "20% bottom",
+      },
+      opacity: 0,
+      scale: 2,
+      duration: 2,
+      ease: "power2.inOut",
+    });
+  }, []);
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <div id="chip" className="flex-center my-20 w-full">
+          <img src={chipImg} alt="chip" width={180} height={180} />
+        </div>
+
+        <div className="flex flex-col items-center">
+          <h2 className="hiw-title">
+            A17 Pro chip. <br />A monster win for gaming.
+          </h2>
+
+          <p className="hiw-subtitle">
+            It's here. The biggest redesign in the history of Apple GPUs.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HowItWorks;
